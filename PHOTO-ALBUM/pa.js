c7c524bdf7efb41962f31148486e204a3d52612a
vanapalabharath <84761500+vanapalabharath@users.noreply.github.com>
2023-03-15 15:15:49 +0530

    Update pa.js

--- PHOTO-ALBUM/pa.js
+++ PHOTO-ALBUM/pa.js
@@ -1,22 +1,8 @@
-const images = document.querySelectorAll('.album img');
-
-images.forEach(img => {
-  img.addEventListener('click', function() {
-
-    if (this.classList.contains('grayscale')) {
-      this.classList.remove('grayscale');
-    } else {
-      this.classList.add('grayscale');
-    }
+function applyFilter(filter) {
+  const images = document.querySelectorAll(".album img");
+  images.forEach((image) => {
+    image.style.filter = filter;
   });
-});
-images.forEach(img => {
-  img.addEventListener('dbclick', function() {
+}
 
-    if (this.classList.contains('brightness')) {
-      this.classList.remove('brightness');
-    } else {
-      this.classList.add('brightness');
-    }
-  });
-});
+
